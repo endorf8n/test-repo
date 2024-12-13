93a4d5dda0c5378b235f809bfb9b05f2a3fa1a4d
Volodymyr Konopka <volodymyr.konopka@gmail.com>
2024-12-13 16:52:26 +0200

    finish home task 6 module js

---
+++ js/homeTask6.js
@@ -0,0 +1,97 @@
+/*===============================================================
+*                      *** Акаунт користувача ***                        
+===============================================================*/
+// const customer = {
+//   username: "Mango",
+//   balance: 24000,
+//   discount: 0.1,
+//   orders: ["Burger", "Pizza", "Salad"],
+//   // Change code below this line
+//   getBalance() {
+//     return this.balance;
+//   },
+//   getDiscount() {
+//     return this.discount;
+//   },
+//   setDiscount(value) {
+//     this.discount = value;
+//   },
+//   getOrders() {
+//     return this.orders;
+//   },
+//   addOrder(cost, order) {
+//     this.balance -= cost - cost * this.discount;
+//     this.orders.push(order);
+//   },
+//   // Change code above this line
+// };
+
+// customer.setDiscount(0.15);
+// console.log(customer.getDiscount()); // 0.15
+// customer.addOrder(5000, "Steak");
+// console.log(customer.getBalance()); // 19750
+// console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+
+/*===============================================================
+*                      *** Склад ***                        
+===============================================================*/
+// class Storage {
+//   #items;
+//   constructor(params) {
+//     this.#items = params;
+//   }
+
+//   getItems() {
+//     return this.#items;
+//   }
+
+//   addItem(newItem) {
+//     this.#items.push(newItem);
+//   }
+
+//   removeItem(itemToRemove) {
+//     this.#items = this.#items.filter((item) => item !== itemToRemove);
+//   }
+// }
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+// storage.removeItem("Scaner");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+/*===============================================================
+*                      *** Конструктор рядків ***                        
+===============================================================*/
+// class StringBuilder {
+//   #value;
+//   constructor(initialValue) {
+//     this.#value = initialValue;
+//   }
+//   getValue() {
+//     return this.#value;
+//   }
+//   padStart(str) {
+//     this.#value = `${str}${this.#value}`;
+//   }
+//   padEnd(str) {
+//     this.#value = `${this.#value}${str}`;
+//   }
+
+//   padBoth(str) {
+//     this.#value = `${str}${this.#value}${str}`;
+//   }
+// }
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
