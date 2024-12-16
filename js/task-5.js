39987a050e1d9d5d15787df416507bb7c33c2db5
Volodymyr Konopka <volodymyr.konopka@gmail.com>
2024-12-16 13:54:40 +0200

    download hw-7

--- js/task-5.js
+++ js/task-5.js
@@ -1,14 +1,5 @@
-const getCommonElements = function (array1, array2) {
-  let newArray = [];
-  for (let i = 0; i < array1.length; i++) {
-    if (array2.includes(array1[i])) {
-      newArray.push(array1[i]);
-    }
-  }
-  return newArray;
-};
-
-console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
-console.log(
-  getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40, 56, 54, 33, 33])
-);
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
